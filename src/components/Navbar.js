78ac26cf7d03b90ee987fcc08b9c16a63af4a3ed
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2023-11-28 22:35:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,31 @@
 import React from 'react'
+import { Route, Routes, Switch, Link } from 'react-router-dom';
+import Quotes from './Quotes';
+import RestCard from './RestCard';
 
 export default function Navbar() {
     return (
         <div>
-            <div>
-                <div className="container flex justify-around items-center">
-                    <div className="flex justify-center items-center">
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT44nWiETCowcsEoKK-zgR6e9qjYt-5a1NPxw&usqp=CAU"
-                            alt="logo"
-                            className=" h-14"
-                        />
-                        <h3 className="text-xl font-bold ml-3">Geek Food</h3>
-                    </div>
+            <div className="container flex justify-around items-center">
+                <div className="flex justify-center items-center">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT44nWiETCowcsEoKK-zgR6e9qjYt-5a1NPxw&usqp=CAU"
+                        alt="logo"
+                        className=" h-14"
+                    />
+                    <h3 className="text-xl font-bold ml-3">Geek Food</h3>
+                </div>
+                <div className='flex flex-row justify-between'>
+                    <Link to="/">Home </Link>
+                    <Link to="/Quotes">Quotes </Link>
+                    <Link to="/RestList">Restaurant </Link>
+                    <Link to="/">Contact </Link>
+                </div>
 
-                    <ul className="flex nav">
-                        <li className="mr-10  hover:bg-green-900">Home</li>
-                        <li className="mr-10  hover:bg-green-900">Quotes</li>
-                        <li className="mr-10  hover:bg-green-900">Restaurants</li>
-                        <li className="mr-10  hover:bg-green-900">Food</li>
-                        <li className="mr-10  hover:bg-green-900">Contact</li>
-                    </ul>
-
-                    <div>
-                        <button className="border-2 bg-green-600 py-2 rounded-xl px-3">
-                            Get Started
-                        </button>
-                    </div>
+                <div>
+                    <button className="border-2 bg-green-600 py-2 rounded-xl px-3">
+                        Get Started
+                    </button>
                 </div>
             </div>
         </div>
